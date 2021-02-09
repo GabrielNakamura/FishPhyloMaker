71ecb68465fada8426bd44161c673c85576990ca
ecoqua <ecoqua@MacPro-2010A.local>
2021-02-09 15:23:59 -0300

    adding possible internal functions

---
+++ R/internal_functions.R
@@ -0,0 +1,22 @@
+#### internal functions 
+
+# check genus in tree
+spp<- as.character(data$s) #especies
+spp_data<- 1:length(spp)
+names(spp_data)<- spp
+insert_spp<- treedata_modif(phy = phylo_order, data = spp_data, warnings = F)$nc$data_not_tree # all species to be inserted
+phylo_order<- phytools::force.ultrametric(tree = phylo_order)
+genres_in_tree<- sub("_.*", "", 
+                     phylo_order$tip.label)[match(sub("_.*", "", 
+                                                      insert_spp), 
+                                                  sub("_.*", "", 
+                                                      phylo_order$tip.label))][!is.na(sub("_.*", "", 
+                                                                                          phylo_order$tip.label)[match(sub("_.*", "", 
+                                                                                                                           insert_spp), 
+                                                                                                                       sub("_.*", "", 
+                                                                                                                           phylo_order$tip.label)
+                                                                                          )
+                                                                                          ]
+                                                      )
+                                                      ]
+species_to_genre<- insert_spp[which(is.na(insert_spp[match(sub("_.*", "", insert_spp), genres_in_tree)]) == FALSE)] #genus that must be added
