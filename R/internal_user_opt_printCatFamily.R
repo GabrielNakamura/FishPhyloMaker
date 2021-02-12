abcb5714f7c043e9780cc8621c087f5e1fdc9871
Gabriel Nakamura <gabriel.nakamura.souza@gmail.com>
2021-02-12 09:25:35 -0300

    new internal function to deal with family level insertions

---
+++ R/internal_user_opt_printCatFamily.R
@@ -0,0 +1,11 @@
+print_cat_family <- function(print_cat, spp, family){
+  cat("in which/between Family species", spp, "must be inserted")
+  cat("\n")
+  for(i in 1:length(print_cat)){
+    cat(i, print_cat[i], sep="\t")
+    cat("\n")
+  }
+  cat("\n")
+  cat(i + 1, "or, to insert in the node corresponding to the Order type:", family, sep= "\t")
+  cat("\n")
+}
