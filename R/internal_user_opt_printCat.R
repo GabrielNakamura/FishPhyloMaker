89059ab6d47766fcefcdd612768b1e90e04af8ff
Gabriel Nakamura <gabriel.nakamura.souza@gmail.com>
2021-02-12 09:24:40 -0300

    adding a new internal function to interactive process in family insertion

--- R/internal_user_opt_printCat.R
+++ R/internal_user_opt_printCat.R
@@ -1,11 +1,11 @@
-print_cat<- function(print_cat, spp, family){
-  cat("in which/between genus species", spp, "must be inserted")
+print_cat <- function(print_cat, spp, family){
+  cat("in which/between Genus species", spp, "must be inserted")
   cat("\n")
   for(i in 1:length(print_cat)){
     cat(i, print_cat[i], sep="\t")
     cat("\n")
   }
   cat("\n")
-  cat(i+1, "or, to insert in family_node type:", family, sep= "\t")
+  cat(i + 1, "or, to insert in the node corresponding to the Family:", family, sep= "\t")
   cat("\n")
 }
