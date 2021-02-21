bd4a83d53b1106c5ab7d85b9a1a1f0f703e35d6f
Gabriel Nakamura <gabriel.nakamura.souza@gmail.com>
2021-02-21 18:54:20 -0300

    temporary script to give name to families and orders

---
+++ R/tmp_naming_families.R
@@ -0,0 +1,33 @@
+
+
+# order for monotipic families
+monotipic_family <- names(unlist(lapply(list_family, function(x) which(length(x) == 1))))
+list_monotipic <- vector(mode = "list", length = length(monotipic_family))
+for(i in 1:length(monotipic_family)){
+  list_monotipic[[i]]<- tryCatch(fishtree::fishtree_taxonomy(rank = monotipic_family[i])[[1]]$taxonomy[[9]],
+                              error = function(e) paste("not.found", "_", monotipic_family[i], sep = ""))
+}
+orders_to_add <- unique(unlist(list_monotipic[- which(sub("_.*", "", unlist(list_monotipic)) == "not.found")]))
+setdiff(rank_order, orders_to_add)
+fishtree::fishtree_taxonomy(rank = monotipic_family[22])
+list_monotipic[which(sub("_.*", "", unlist(list_monotipic)) == "not.found")]
+match("Characidae", phylo_order$node.label)
+
+
+fishtree::fishtree_phylogeny(rank = "Incertae sedis in Eupercaria")
+fishtree::fishtree_taxonomy(rank = "Teletubiidae")[[1]]$taxonomy[[9]]
+fishtree::fishtree_taxonomy(rank = "Zanclidae")$Zanclidae$taxonomy$order
+
+
+which(fishtree_phylogeny(rank = fishtree::fishtree_taxonomy(rank = "Zanclidae")$Zanclidae$taxonomy$order)$tip.label == "Zanclus_cornutus")
+
+fishtree::fishtree_taxonomy(rank = "Zanclidae")$Zanclidae$taxonomy$order)
+
+fishtree::fishtree_phylogeny(rank = "Synbranchidae")
+fishtree::fishtree_phylogeny(rank = "Zanclidae")
+fishtree::fishtree_phylogeny(rank = "Teletubidae")
+fishtree::fishtree_phylogeny(rank = "Scaridae")
+fishtree::fishtree_phylogeny(rank = "Acanthuriformes")
+
+
+fishtree::fishtree_taxonomy(rank = "Pholidichthyiformes")
