8232dce32be201a333bfa71f32d9fb92b9c88031
Gabriel Nakamura <gabriel.nakamura.souza@gmail.com>
2021-04-15 16:39:32 -0300

    removing naming procedure

--- R/internal_orderInsert_function.R
+++ R/internal_orderInsert_function.R
@@ -1,13 +1,12 @@
-insert_allbaseOrd <- function(phylo = phylo_order, data_round3 = data_exRound3){
+#' Internal add species to order node
+#'
+#' @param phylo phylogenetic tree
+#' @param data_round3 data frame
+#'
+#' @return phylogenetic tree
+#'
+insert_allbaseOrd <- function(phylo = phylo_order, data_round3 = data_exRound3, list_names_ord = list_order){
   order_add_round3 <- unique(data_round3$o)
-  for (i in 1:length(order_add_round3)) {
-    list_names_round3 <- list_order[[which(names(list_order) == 
-                                             order_add_round3[i])]]
-    phylo <- ape::makeNodeLabel(phylo, 
-                                      "u", nodeList = list(Ord_name = list_names_round3))
-    phylo$node.label[which(phylo$node.label == 
-                                   "Ord_name")] <- order_add_round3[i]
-  }
   for(i in 1:nrow(data_round3)){
     node_order_pos <- which(c(phylo$tip.label, 
                               phylo$node.label) == data_round3$o[i])
