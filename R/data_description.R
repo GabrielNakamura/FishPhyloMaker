17749825f3fdcf1ea8ff9870ba1423989a93e94d
SeuNomeDeUsuario <seuMail@example.br>
2021-09-07 11:47:55 -0300

    updating url in data description

--- R/data_description.R
+++ R/data_description.R
@@ -4,7 +4,7 @@
 #' Parana and Paraguay river Basins
 #'
 #' @format A data frame with 20 rows and 61 variables:
-#' @references \url{https://www.scielo.br/j/ni/a/jWsfj3ZDr6qBbMyg5kfczrB/?lang=en#}
+#' @source Article published in Neotropical Ichthyology  \doi{10.1590/1982-0224-2020-0126 }
 #
 "neotropical_comm"
 
