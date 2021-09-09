e3902f6a1eece60096796cee1295926a5bcdaea1
SeuNomeDeUsuario <seuMail@example.br>
2021-09-09 19:02:36 -0300

    adding a new example dataset

--- R/data_description.R
+++ R/data_description.R
@@ -4,7 +4,7 @@
 #' Parana and Paraguay river Basins
 #'
 #' @format A data frame with 20 rows and 61 variables:
-#' @source Article published in Neotropical Ichthyology  \doi{10.1590/1982-0224-2020-0126 }
+#' @source Article published in Neotropical Ichthyology  \doi{10.1590/1982-0224-2020-0126}
 #
 "neotropical_comm"
 
@@ -17,3 +17,12 @@
 #' @references \url{https://www.nature.com/articles/sdata2017141}
 # 
 "spp_afrotropic"
+
+#' Data frame with species names needed to assemble the phylogenetic tree
+#'
+#' A data frame that can be directly used in FishPhyloMaker to obtain a phylogenetic tree
+#'
+#' @format A data frame with taxonomic classification (species, family and order) of 45 species
+#' @references Species that make up the dataset in the paper published in Neotropical Ichthyology  \doi{10.1590/1982-0224-2020-0126}
+# 
+"taxon_data_PhyloMaker"
