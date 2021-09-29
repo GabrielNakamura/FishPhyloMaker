82b1558294f2ea226f879f2e83da1a9a2cd5e999
SeuNomeDeUsuario <seuMail@example.br>
2021-09-29 16:48:37 -0300

    updating pd deficit removing picante

--- R/PD_deficit.R
+++ R/PD_deficit.R
@@ -9,9 +9,13 @@
 #'     default is "Congeneric_insertion".
 #'
 #' @return A vector containing four values: 
+#' 
 #'     - Amount phylogenetic information present in the tree before insertions (PDintree)
+#'     
 #'     - Amount of phylogenetic information inserted in the tree (PDdeficit)
+#'     
 #'     - Total Phylogenetic information of the tree (PDtotal)
+#'     
 #'     - A ratio calculated as PDdeficit/PDtotal (Darwinian_deficit)
 #' @export
 #'
@@ -29,24 +33,16 @@ PD_deficit <- function(phylo, data, level = "Congeneric_insertion"){
   
   if(all(!is.na(match(phylo$tip.label, names_exclude))) == TRUE){
     PD_present <- sum(phylo$edge.length)
-    comm_total <- matrix(data = rep(1, length(phylo$tip.label)),
-                         ncol = length(phylo$tip.label),
-                         nrow = 1, dimnames = list("comm1", phylo$tip.label))
-    PD_total <- picante::pd(samp = comm_total, tree = phylo, include.root = TRUE)$PD
     PD_level <- 0
+    PD_total <- sum(PD_present, PD_level)
     Darwinian_deficit <- PD_level/PD_total
     res <- c(PD_present, PD_level, PD_total, Darwinian_deficit)
     names(res) <- c("PDintree", "PDdeficit", "PDtotal", "Darwinian_deficit")
     return(res)
-    
   } else{
     exclude <- ape::drop.tip(phylo, tip = names_exclude)$tip.label
     phylo_present <- ape::drop.tip(phylo, tip = exclude)
-    comm_present <- matrix(data = rep(1, length(phylo_present$tip.label)), nrow = 1, 
-                           ncol = length(phylo_present$tip.label), 
-                           dimnames = list("comm1", phylo_present$tip.label))
-    PD_present <- picante::pd(samp = comm_present, tree = phylo_present, include.root = TRUE)$PD
-    # PD_present <- sum(phylo_present$edge.length)
+    PD_present <- sum(phylo_present$edge.length)
     if(length(level) == 1){
       level_exclude <- ape::drop.tip(phylo, tip = phylo$tip.label[na.omit(match(data[which(data$insertions == level), "s"], 
                                                                                 phylo$tip.label))])$tip.label
@@ -55,16 +51,8 @@ PD_deficit <- function(phylo, data, level = "Congeneric_insertion"){
       level_exclude <- ape::drop.tip(phylo, data[!is.na(match(data$insertions, level)), "s"])$tip.label 
     }
     phylo_level <- ape::drop.tip(phylo, level_exclude)
-    comm_level <- matrix(data = rep(1, length(phylo_level$tip.label)), nrow = 1, 
-                         ncol = length(phylo_level$tip.label), 
-                         dimnames = list("comm1", phylo_level$tip.label))
-    PD_level <- picante::pd(samp = comm_level, tree = phylo_level, include.root = TRUE)$PD
-    # PD_level <- sum(phylo_level$edge.length)
-    comm_total <- matrix(data = rep(1, length(phylo$tip.label)),
-                         ncol = length(phylo$tip.label),
-                         nrow = 1, dimnames = list("comm1", phylo$tip.label))
-    PD_total <- picante::pd(samp = comm_total, tree = phylo, include.root = TRUE)$PD
-    #PD_total <- sum(PD_present, PD_level)
+    PD_level <- sum(phylo_level$edge.length)
+    PD_total <- sum(PD_present, PD_level)
     Darwinian_deficit <- PD_level/PD_total
     res <- c(PD_present, PD_level, PD_total, Darwinian_deficit)
     names(res) <- c("PDintree", "PDdeficit", "PDtotal", "Darwinian_deficit")
