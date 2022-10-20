12f1428c83f5a30cc0cd21348d02275543349365
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2022-10-19 20:31:45 -0500

    grafting species at order level

--- updating_name.R
+++ updating_name.R
@@ -1,4 +1,4 @@
-# Data
+# Data and packages
 
  library(ape)
  library(devtools)
@@ -61,8 +61,7 @@ for(i in 1:length(position_monotipic)){ # naming tree nodes
   tree_update$node.label[position_monotipic[[i]] - ape::Ntip(tree_update)] <- names(position_monotipic)[i]
 }
 
-
-# adding congeneric species
+# congeneric insertion ----------------------------------------------------
 
 data <- spp_df <- read.table('/Users/gabriel.nakamuradesouza/Library/CloudStorage/OneDrive-Personal/Manuscritos/Darwinian_shortfalls/data/taxa_table.txt', header = TRUE)
 tree_update_genus <- tree_update
@@ -70,9 +69,14 @@ pb_congeneric <- progress::progress_bar$new(format = "Adding congeneric species 
                                             total = length(data$s), clear = FALSE, 
                                             width = 60, current = "<", incomplete = ">", 
                                             complete = ">")
-for(i in 1:length(data$s)){
+
+genus_data <- sub("_.*", "", data$s)
+genus_tree <- sub("_.*", "", tree_update_genus$tip.label)
+genus_in_tree <- genus_data[genus_data %in% genus_tree]
+insert_genus <- data[genus_data %in% genus_tree, "s"]
+for(i in 1:length(insert_genus)){
   tree_update_genus <- phytools::add.species.to.genus(tree = tree_update_genus, 
-                                                      species = data$s[i])
+                                                      species = insert_genus[i])
   pb_congeneric$tick()
 }
 
@@ -83,7 +87,7 @@ families_tree <- families_2[!is.na(match(families_2, tree_update_genus$node.labe
 families_not_tree <- families_2[is.na(match(families_2, tree_update_genus$node.label))]
 data_round_2 <- data_round_2[data_round_2$f %in% families_tree, ] # only species with data in tree
 
-# family insertions - all in family nodes
+# family insertion --------------------------------------------------------
 
 tree_update_family <- tree_update_genus
 is.ultrametric(tree_update_family) # check here if tree is ultrametric
@@ -172,9 +176,78 @@ while (length(spp_to_add_round2) >= 1) {
 tree_update_order <- tree_update_family
 
 data_round_3 <- data[which(is.na(match(data$s, tree_update_family$tip.label)) == TRUE), ]
-orders_3 <- unique(data_round_2$o)
-families_tree <- families_2[!is.na(match(families_2, tree_update_genus$node.label))]
-families_not_tree <- families_2[is.na(match(families_2, tree_update_genus$node.label))]
-data_round_2 <- data_round_2[data_round_2$f %in% families_tree, ] # only species with data in tree
+
+# order species
+orders <-  tax[which(tax$rank == "order"), ]
+orders_no_incertae <- orders[-grep("Incertae sedis in", orders$name), ] # removing incertae sedis
+spp_order <- lapply(orders_no_incertae$name, function(x) fishtree::fishtree_taxonomy(ranks = x)[[1]]$sampled_species)
+names(spp_order) <- orders_no_incertae$name
+spp_order <- lapply(spp_order, function(x) gsub(" ", "_", x))
 
 
+# naming orders 
+
+not_samp_orders <- names(unlist(lapply(spp_order, function(x) which(length(x) == 0)))) # fdmilies not sampled in phylogeny
+monotip_orders <-  names(unlist(lapply(spp_order, function(x) which(length(x) == 1)))) # monotipic families in the tree
+one_spp <- c(not_samp_orders, monotip_orders)
+order_names_tree <- spp_order[-match(one_spp, names(spp_order))]
+position_order <- 
+  lapply(order_names_tree, function(x){
+    node_anc <- ape::getMRCA(phy = tree_update_order, tip = x)
+  })
+for(i in 1:length(position_order)){ # naming tree nodes
+  tree_update_order$node.label[position_order[[i]] - ape::Ntip(tree_update_order)] <- names(position_order)[i]
+}
+
+monotipic_list_order <- spp_order[match(monotip_orders, names(spp_order))]
+
+for(i in 1:length(monotip_orders)){ 
+  spp_monotipic <- spp_order[match(monotip_orders[i], names(spp_order))][[1]]
+  
+  spp_singleton <- tree_update_order$tip.label[match(spp_monotipic, tree_update_order$tip.label)]
+  spp_singleton_add <- paste(sub("_.*", "", spp_singleton), 
+                             "_", "singleton", sep = "")
+  tree_update_order <- phytools::add.species.to.genus(tree = tree_update_order, 
+                                                species = spp_singleton_add)
+  monotipic_list_order[[i]] <- c(spp_singleton, spp_singleton_add)
+}
+
+# naming monotipic orders
+
+position_monotipic <- 
+  lapply(monotipic_list_order, function(x){
+    node_anc <- ape::getMRCA(phy = tree_update_order, tip = x)
+  })
+for(i in 1:length(position_monotipic)){ # naming tree nodes
+  tree_update_order$node.label[position_monotipic[[i]] - ape::Ntip(tree_update_order)] <- 
+    names(position_monotipic)[i]
+}
+
+# adding species in order node
+
+node_number_order <- lapply(data_round_3$o, function(x) which(c(tree_update_order$tip.label, 
+                                                                 tree_update_order$node.label) == x)) # finding the names of orders in tree
+pos_no_order <- which(lapply(node_number_order, function(x) length(x) == 0) == TRUE) # species without order in tree
+
+data_round_3_order <- data_round_3[-pos_no_order, ] # removing species with no order in the tree
+node_number_order_tree <- node_number_order[-pos_no_order] # removing orders with no species
+ord_insertion <- order(unlist(node_number_order_tree)) # setting the order of insertion of nodes to keep the shallow nodes first and the deeper nodes last
+node_number_order_tree <- node_number_order_tree[ord_insertion]
+data_round_3_order <- data_round_3_order[ord_insertion, ]
+pb_order <- progress::progress_bar$new(format = "Adding species to order nodes [:bar] :percent", 
+                                       total = length(node_number_order_tree), clear = FALSE, 
+                                       width = 60, current = "<", incomplete = ">", 
+                                       complete = ">")
+
+for(i in 1:length(node_number_order_tree)){
+  tree_update_order <-
+    phytools::bind.tip(tree = tree_update_order, 
+                       tip.label = data_round_3_order$s[i], 
+                       where = node_number_order_tree[i], 
+                       position = 0) # position can be a value provided by the user
+  # identify if there is any species from the same genus
+  pb_order$tick()
+}
+data_final_inserted <- data$s[data$s %in% tree_update_order$tip.label]
+tree_final <- keep.tip(phy = tree_update_order, tip = data_final_inserted)
+length(tree_final$tip.label) - dim(data)[1]
